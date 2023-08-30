2bc7b5a6f7ea2c379194a63db9acac08bcb75c78
Hammad Saeed <87358915+hammadsaeed0@users.noreply.github.com>
2023-08-31 03:55:12 +0500

    Test2

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const express = require('express')
-const path= require('path')
 const app = express()
 const PORT = process.env.PORT || 3000
-
-app.set('view engine', 'ejs')
-
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.send("Hey")
